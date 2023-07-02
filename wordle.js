137d9c9ab8f49e5ca3d75e5a91d3e368de915491
Yves Bonneau <yves.bonneau2004@gmail.com>
2023-07-02 01:45:12 +0100

    Added keyboard

--- wordle.js
+++ wordle.js
@@ -1,6 +1,7 @@
 const ANSWER_LENGTH = 5;
 const NUM_ANSWERS = 6;
 const letters = document.querySelectorAll(".game-word");
+const keys = document.querySelectorAll(".keyboard");
 
 async function init() {
   let rowNumber = 0;
@@ -12,6 +13,8 @@ async function init() {
   );
   const wordJSON = await wordAPI.json();
   const wordOfDay = wordJSON.word;
+
+  console.log(wordOfDay);
 
   document.addEventListener("keyup", function (event) {
     if (gameOver) {
@@ -25,6 +28,18 @@ async function init() {
       handleBackSpace();
     } else if (isLetter(letter) == true) {
       handleLetter(letter);
+    }
+    console.log(currentWord);
+  });
+
+  document.addEventListener("click", function (event) {
+    pressed = event.target.innerHTML;
+    if (pressed == "ENTER") {
+      handleEnter();
+    } else if (pressed == "DEL") {
+      handleBackSpace();
+    } else {
+      handleLetter(pressed.toLowerCase());
     }
   });
 
@@ -72,7 +87,7 @@ async function init() {
                 letter.classList.add("letter-correct");
               };
             })(letters[rowNumber * ANSWER_LENGTH + i]),
-            82 * i
+            250 * i
           );
           letterMap.set(currentWord[i], letterMap.get(currentWord[i]) - 1);
         }
@@ -97,7 +112,7 @@ async function init() {
                   letter.classList.add("letter-present");
                 };
               })(letters[rowNumber * ANSWER_LENGTH + i]),
-              82 * i
+              250 * i
             );
             letterMap.set(currentWord[i], letterMap.get(currentWord[i]) - 1);
           } else {
@@ -107,7 +122,7 @@ async function init() {
                   letter.classList.add("letter-wrong");
                 };
               })(letters[rowNumber * ANSWER_LENGTH + i]),
-              82 * i
+              250 * i
             );
           }
         }
